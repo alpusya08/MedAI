a5ca10737f175e9c697446135def894786ef7861
alpusya08 <mosh0303@mail.ru>
2026-01-17 18:12:17 +0500

    initial commit

---
+++ src/test/java/com/medai/MedAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
